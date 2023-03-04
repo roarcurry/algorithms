68aa456956e70d5606cb4b6e11f4733d67d90eeb
rocurry <675478140@qq.com>
2023-03-04 16:08:18 +0800

    feat(leetcode): 0

---
+++ leetcode/0.js
@@ -0,0 +1,13 @@
+/**
+ * [0. 模板](https://leetcode.cn/xxx)
+ * @param {number} num
+ * @return {number}
+ */
+var fun = function (num) {
+    return num;
+};
+
+const { expect } = require('chai');
+it('0', () => {
+    expect(fun(1)).to.eq(1);
+});
